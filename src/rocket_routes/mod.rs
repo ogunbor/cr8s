301bf13a82e605c103453e7ecfd5cfa50430bf4d
mompei <ogunborjerry@gmail.com>
2023-12-06 11:45:43 +0100

    Login endpoint

--- src/rocket_routes/mod.rs
+++ src/rocket_routes/mod.rs
@@ -1,11 +1,11 @@
-pub mod rustaceans;
-pub mod crates;
-
 use diesel::PgConnection;
 use rocket::http::Status;
 use rocket::response::status::Custom;
 use rocket::serde::json::{serde_json::json, Value};
 
+pub mod rustaceans;
+pub mod crates;
+pub mod authorization;
 
 #[rocket_sync_db_pools::database("postgres")]
 pub struct DbConn(PgConnection);
